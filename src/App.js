ed120cfdb3df5de56166cbf39a6d00a24d58d96e
tonymadjid <tonymadjid@yahoo.com>
2022-06-30 22:15:53 +0700

    Tantangan Intro React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-
+// import './App.css';
+import Header from './components/Header.js';
+import Contact from './components/Contact.js';
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
-const App = () => {
+const App = ({props}) => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      <Contact data={contacts[0]}/>
+      <Contact data={contacts[1]}/>
+      <Contact data={contacts[2]}/>
+      <Contact data={contacts[3]}/>
+      <Contact data={contacts[4]}/>
+      <Contact data={contacts[5]}/>
     </div>
   );
 }
