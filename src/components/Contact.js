d7297e24ea69888c1193027d4a09f310eb345a2a
tonymadjid <tonymadjid@yahoo.com>
2022-06-30 22:12:50 +0700

    Tantangan Ontro React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className="row" key={props.data.name}>
+          <div className="box-contact">
+            <img className="img" src={props.data.photo} />
+            <div className="box-description">
+              <h3 className="name">{props.data.name}</h3>
+              <h3 className="phone">{props.data.phone}</h3>
+              <h3 className="email">{props.data.email}</h3>
+          </div>
+        </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
-    return (
-        <></>
+    return ( <
+        div className = 'header' >
+        <
+        center >
+        <
+        h1 > Call a Friend < /h1> <
+        h4 > Your friendly contact app < /h4> <
+        h4 class = "dot" > .................................................................................... < /h4><br / > < br / > < /
+        center >
+        <
+        /div>
     )
 }
 
